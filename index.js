eae5393f80443d5dc5c10e9b55472440adb27b4c
misqlight <vimefeed@gmail.com>
2023-09-03 03:36:48 +0300

    Add JSDoc types

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
 const http = require('node:https');
 
+/** @typedef {"BasicMath"|"prealgebra"|"algebra"|"trigonometry"|"precalculus"|"calculus"|"statistics"|"finitemath"|"linearalgebra"|"chemistry"|"physics"} Subject */
+
+/**
+ * @typedef {object} Topic
+ * @property {number} id - Unique ID of the topic
+ * @property {number} score - Number from 0 to 1. Probability that this topic was meant
+ * @property {string} text - Topic text
+ */
+
+/**
+ * @typedef {object} Message
+ * @property {string} content - Content of the message (with HTML tags)
+ * @property {"mathway"|"message"|"autoresolve"|"greeting"} genre - Genre/Type of the message
+ * @property {number} timestamp - Timestamp of the message
+ */
+
+/** 
+ * @typedef {object} TopicsResponse
+ * @property {"topicsResponse"} type - Response type
+ * @property {Topic[]} topics - Array of suggested topics
+ * 
+*/
+
+/**
+ * @typedef {object} MessagesResponse
+ * @property {"messagesResponse"} type - Response type
+ * @property {Message[]} messages - Messages related to the provided request
+ */
+
+/** @typedef {TopicsResponse|MessagesResponse} MathwayResponse */
+
 /** @param {http.RequestOptions} options */
 async function sendRequest(options, data) {
     return new Promise((resolve, reject) => {
@@ -14,6 +45,15 @@ async function sendRequest(options, data) {
     });
 }
 
+/**
+ * Submit an expression to MathWay
+ * 
+ * @public
+ * @param {string} expression - Expression to submit (in LaTeX)
+ * @param {Subject} subject - Answers subject
+ * @param {string} [language] - 2-letter code of answers language
+ * @returns {Promise<MathwayResponse>}
+*/
 async function submit(expression, subject, language) {
     return new Promise(async (resolve, reject) => {
         const body = {
