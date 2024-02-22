af7b988ff4b8ba4fe6113b07dc2e66281b1aac64
misqlight <vimefeed@gmail.com>
2024-02-22 16:49:07 +0300

    add both ES6 & CommonJS support

---
+++ wrapper.js
@@ -0,0 +1,5 @@
+import * as cjs from "./index.cjs";
+export const getGlossaryTerm = cjs.getGlossaryTerm;
+export const greet = cjs.greet;
+export const getTopicResult = cjs.getTopicResult;
+export const submit = cjs.submit;
